b17a7b18586b72cb35a027dea87a96d93c51ab02
Gareth Carter <gareth.carter@outlook.com>
2022-01-31 18:55:37 +0000

    added sass module to home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
+import styles from '../Styles/Home.module.scss';
+
 export function Home() {
     return (
         <>
-            <section>
+            <section className={styles.homeSection}>
                 <h5>SO, YOU WANT TO TRAVEL TO</h5>
                 <h1>SPACE</h1>
                 <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
             </section>
-            <button id="explore-circle">
+            <button className={styles.exploreCircle}>
                 <h4>EXPLORE</h4>
             </button>
         </>
