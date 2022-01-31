df4ce7d9ab2f8528e514015d84530a06599927a0
Gareth Carter <gareth.carter@outlook.com>
2022-01-31 18:42:54 +0000

    added sass modules to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,4 @@
+import styles from '../Styles/Nav.module.scss'
 import logo from '../Assets/shared/logo.svg';
 import hamburgerOpen from '../Assets/shared/icon-hamburger.svg';
 import hamburgerClosed from '../Assets/shared/icon-close.svg'
@@ -5,11 +6,11 @@ import hamburgerClosed from '../Assets/shared/icon-close.svg'
 
 export function Nav() {
     return (
-        <nav>
-            <img id="logo" src={logo} alt="Space Tourism Logo" />
-            <button className="nav-menu-button">
-                <img id="hamburger-open" src={hamburgerOpen} alt="Hamburger icon" />
-                <img id="hamburger-closed" src={hamburgerClosed} alt="Closed icon" />
+        <nav className={styles.nav}>
+            <img className={styles.logo} src={logo} alt="Space Tourism Logo" />
+            <button>
+                <img className={styles.hamburgerOpen} src={hamburgerOpen} alt="Hamburger icon" />
+                <img className={styles.hamburgerClosed} src={hamburgerClosed} alt="Closed icon" />
             </button>
             
             
