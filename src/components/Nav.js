d7d96df59ca4bd55259074344c84c821424273b9
Gareth Carter <gareth.carter@outlook.com>
2022-02-01 17:10:38 +0000

    added more styles for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,18 @@ import logo from '../Assets/shared/logo.svg';
 import hamburgerOpen from '../Assets/shared/icon-hamburger.svg';
 import hamburgerClosed from '../Assets/shared/icon-close.svg'
 
+//links to be done when routing is complete
 
 export function Nav() {
     return (
         <nav className={styles.nav}>
             <img className={styles.logo} src={logo} alt="Space Tourism Logo" />
+            <span className={styles.line}></span>
             <div className={styles.navMenu}>
-                <a>HOME</a>
-                <a>DESTINATION</a>
-                <a>CREW</a>
-                <a>TECHNOLOGY</a>
+                <a><span>00</span>HOME</a>
+                <a><span>01</span>DESTINATION</a>
+                <a><span>02</span>CREW</a>
+                <a><span>03</span>TECHNOLOGY</a>
             </div>
             <button className={styles.navButton}>
                 <img className={styles.hamburgerOpen} src={hamburgerOpen} alt="Hamburger icon" />
