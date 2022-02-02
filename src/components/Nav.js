07bcccfedd3c67198abc315a4e746a286ac82efb
Gareth Carter <gareth.carter@outlook.com>
2022-02-02 10:03:43 +0000

    Added additional routing functionality and styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,8 @@ import styles from '../Styles/Nav.module.scss'
 import logo from '../Assets/shared/logo.svg';
 import hamburgerOpen from '../Assets/shared/icon-hamburger.svg';
 import hamburgerClosed from '../Assets/shared/icon-close.svg'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 
 //links to be done when routing is complete
 
@@ -12,10 +13,11 @@ export function Nav() {
             <img className={styles.logo} src={logo} alt="Space Tourism Logo" />
             <span className={styles.line}></span>
             <div className={styles.navMenu}>
-                <Link to="/"><span>00</span>HOME</Link>
-                <Link to="destination"><span>01</span>DESTINATION</Link>
-                <Link to="crew"><span>02</span>CREW</Link>
-                <Link to="technology"><span>03</span>TECHNOLOGY</Link>
+                <NavLink className={( { isActive }) => isActive ? styles.selected : null} to="/"><span>00</span>HOME</NavLink>
+                
+                <NavLink className={( { isActive }) => isActive ? styles.selected : null} to="destination"><span>01</span>DESTINATION</NavLink>
+                <NavLink className={( { isActive }) => isActive ? styles.selected : null} to="crew"><span>02</span>CREW</NavLink>
+                <NavLink className={( { isActive }) => isActive ? styles.selected : null} to="technology"><span>03</span>TECHNOLOGY</NavLink>
             </div>
             <button className={styles.navButton}>
                 <img className={styles.hamburgerOpen} src={hamburgerOpen} alt="Hamburger icon" />
