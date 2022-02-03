2957cb3c7cddf330b8372fbe6a4b74dca9c48446
Gareth Carter <gareth.carter@outlook.com>
2022-02-03 13:35:48 +0000

    added hover effect for explore button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,6 +13,7 @@ export function Home() {
                 <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
             </section>
             <button onClick={() => navigate('destination')} className={styles.exploreCircle}>
+                <span class={styles.hoverCircle}></span>
                 <h4>EXPLORE</h4>
             </button>
         </>
